34fecbafbcf297c330621db806c7980de09d84e9
MAU-A <72873192+MAU-A@users.noreply.github.com>
2025-03-23 14:22:24 +0300

    Add files via upload

---
+++ fake.name.py
@@ -0,0 +1,75 @@
+import file_operations
+
+
+from faker import Faker
+
+
+import random
+
+
+fake = Faker("ru_RU")
+
+
+letters_mapping = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+skills = ["Стремительный прыжок", "Электрический выстрел",
+          "Ледяной удар", "Стремительный удар", "Кислотный взгляд",
+          "Тайный побег", "Ледяной выстрел", "Огненный заряд"]
+
+def main():
+    
+    rep_skills = []
+
+    for skill in skills:
+        rep_skill = ""
+        for char in skill:
+            rep_skill += letters_mapping.get(char)
+        rep_skills.append(rep_skill)
+
+    for i in range(1, 11):
+        random_skills = random.sample(rep_skills, 3)
+
+        context = {
+            "first_name": fake.first_name_male(),
+            "last_name": fake.last_name_male(),
+            "job": fake.job(),
+            "town": fake.city(),
+            "strength": random.randint(3, 18),
+            "agility": random.randint(3, 18),
+            "endurance": random.randint(3, 18),
+            "intelligence": random.randint(3, 18),
+            "luck": random.randint(3, 18),
+            "skill_1": random_skills[0],
+            "skill_2": random_skills[1],
+            "skill_3": random_skills[2],
+        }
+
+        file_operations.render_template("template.svg", f"cards/result_{i}.svg", context)
+
+
+if __name__ == '__main__':
+    main()
